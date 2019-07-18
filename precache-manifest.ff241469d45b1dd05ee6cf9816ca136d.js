cd98e8b8528daea3f31c2436cf30aff0167d2023
sunilale0 <sunilale0@gmail.com>
2019-07-18 01:25:42 -0400

    Updates

---
+++ precache-manifest.ff241469d45b1dd05ee6cf9816ca136d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f92e14d8d7b6733257ec898cd3a9a2",
+    "url": "/ProjectX/index.html"
+  },
+  {
+    "revision": "03bd68bcf51509f7f86f",
+    "url": "/ProjectX/static/css/main.c89884d9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ProjectX/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "03bd68bcf51509f7f86f",
+    "url": "/ProjectX/static/js/main.ee302fb9.chunk.js"
+  },
+  {
+    "revision": "355828b71b3c7b1b1771",
+    "url": "/ProjectX/static/js/runtime~main.7b2e4054.js"
+  }
+]);
