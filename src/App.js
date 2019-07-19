33a3d643a6594f558d4aee07f5059edccd3fa82a
sunilale0 <sunilale0@gmail.com>
2019-07-19 01:47:14 -0400

    added new component Header and working on homepage display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Routes from './Routes';
 import Nav from './Components/Nav/Nav'
+import Header from './Components/Header/header'
 
 const App = (props) => {
+ 
+  
   return (
     <div>
+      <Header/>
       <Nav />
       <Routes />
     </div>
