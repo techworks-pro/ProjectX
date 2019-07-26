cdd3a9b5a8e98fd0cb6480b9dc4fdb7769200ef7
Rinchen Lama <rinchenlama0075@gmail.com>
2019-07-26 01:19:17 -0400

    added quiz component ith bugs please help debug

---
+++ src/Components/AllApps/Quiz/Quiz.js
@@ -0,0 +1,90 @@
+let Quiz = [
+  { id: 1, Question: "How do you write 'hello' in the console in JS?", key: 1 },
+  { id: 2, Question: "How do you write 'hello' in the console in JS?", key: 1 },
+  {
+    id: 3,
+    Question: "How do you write 'hnynyny' in the console in JS?",
+    key: 1
+  },
+  { id: 4, Question: "How do you write 'hello' in the console in JS?", key: 1 },
+  { id: 5, Question: "How do you write 'hello' in the console in JS?", key: 1 },
+  { id: 6, Question: "How do you write 'hello' in the console in JS?", key: 1 },
+  { id: 7, Question: "How do you write 'hello' in the console in JS?", key: 1 },
+  { id: 8, Question: "How do you write 'hello' in the console in JS?", key: 1 },
+  { id: 9, Question: "How do you write 'hello' in the console in JS?", key: 1 }
+];
+
+let Options = [
+  {
+    id: 1,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 2,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 3,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 4,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 5,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 6,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 7,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 8,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 9,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  },
+  {
+    id: 10,
+    Option1: "Console.log('hello')",
+    Option2: "ConsoleLog('hello')",
+    Option3: "ConsoleLog('hello')",
+    Option4: "ConsoleLog('hello')"
+  }
+];
+
+module.exports = { Quiz, Options };
