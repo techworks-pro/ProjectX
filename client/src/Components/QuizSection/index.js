45b6c7a2092fa3e2b7d4a8e49b0af569873c3067
Rinchen Lama <rinchenlama0075@gmail.com>
2019-08-03 01:18:35 -0400

    working quiz module. need to fix score not updating.

--- client/src/Components/QuizSection/index.js
+++ client/src/Components/QuizSection/index.js
@@ -1,20 +1,41 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './quiz.css';
 
 class Quiz extends Component {
   constructor() {
     super();
     this.state = {
       questions: [],
+      questionSet: [],
+      questionNum: 1,
+      selectedOption: '',
+      score: 0,
       isLoaded: false
     };
   }
+  selectOption = event => {
+    this.setState({ selectedOption: event.target.name });
+    this.nextQuestion();
+  };
+
+  nextQuestion = event => {
+    if (
+      this.state.selectedOption ==
+      this.state.questions[this.state.questionNum].answer
+    ) {
+      console.log('right answer!');
+      this.setState({ score: this.state.score + 1 });
+    }
+    console.log('score: ', this.state.score);
+
+    this.setState({ questionNum: this.state.questionNum + 1 });
+  };
 
   async componentDidMount() {
     try {
       //axios build in json()
       const res = await axios.get(`/quizs`);
-      console.log(res);
       this.setState({
         questions: res.data,
         isLoaded: true
@@ -26,31 +47,56 @@ class Quiz extends Component {
 
   render() {
     return this.state.isLoaded ? (
-      <div> {this.state.questions.map(el=>{
-        return <div>
-                   <li>
-          {el.question}
-          </li>
-          <li>
-          {el.option1}
-          </li>
-          <li>
-          {el.option2}
-          </li>
-          <li>
-          {el.option3}
-          </li>
-          <li>
-          {el.option4}
-          </li>
-
+      this.state.questions.length !== this.state.questionNum ? (
+        <div className='QuizBox'>
+          <div className='QuestionBox'>
+            {this.state.questions[this.state.questionNum].question}
           </div>
-         
-          
-      })}</div>
+          <div className='OptionBox'>
+            <button
+              class='button'
+              onClick={this.selectOption}
+              name={this.state.questions[this.state.questionNum].option1}
+            >
+              {this.state.questions[this.state.questionNum].option1}
+            </button>
+
+            <button
+              class='button'
+              onClick={this.selectOption}
+              name={this.state.questions[this.state.questionNum].option2}
+            >
+              {this.state.questions[this.state.questionNum].option2}
+            </button>
+
+            <button
+              class='button'
+              onClick={this.selectOption}
+              name={this.state.questions[this.state.questionNum].option3}
+            >
+              {this.state.questions[this.state.questionNum].option3}
+            </button>
+            <button
+              class='button'
+              onClick={this.selectOption}
+              name={this.state.questions[this.state.questionNum].option4}
+            >
+              {this.state.questions[this.state.questionNum].option4}
+            </button>
+
+            {/* <button type='button' onClick={this.nextQuestion()}>
+            Next
+          </button> */}
+          </div>
+        </div>
+      ) : (
+        <div className='QuizEnd'>end of quiz!</div>
+      )
     ) : (
       <div>Loading...</div>
     );
+
+    // );
   }
 }
 
