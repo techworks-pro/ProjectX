b132f90405a1fd2adb369421af8e40ac045d2476
Student <jsitbon@qcc.cuny.edu>
2019-08-05 18:00:40 -0400

    styled the forms and more work on css

--- client/src/Components/QuizSection/QuizAdmin.js
+++ client/src/Components/QuizSection/QuizAdmin.js
@@ -18,7 +18,7 @@ class QuizAdmin extends Component {
     event.preventDefault();
     const { Question, Option1, Option2, Option3, Option4, Answer } = this.state;
     console.log("AddQuiz fired!", this.state);
-    
+
     try {
       await axios.post(
         `/api/quizs/add`, {
@@ -31,78 +31,89 @@ class QuizAdmin extends Component {
           answer: Answer
         }
       );
-          this.setState(
-                {    
-                    Question:'',
-                    Option1:'',
-                    Option2:'',
-                    Option3: '',
-                    Option4: '',
-                    Answer: ''
-              }  
-            )
-            console.log('AddQuiz successful!')
+      this.setState(
+        {
+          Question: '',
+          Option1: '',
+          Option2: '',
+          Option3: '',
+          Option4: '',
+          Answer: ''
+        }
+      )
+      console.log('AddQuiz successful!')
     } catch (error) {
       console.error(error)
     }
-      
- 
+
+
   };
   render() {
     return (
-      <div className='QuizAdminBody'>
-        <div>Insert Quiz</div>
-        <div className='QuizTable'>
-          <tr>
-            <th>Question</th>
-            <th>Option1</th>
-            <th>Option2</th>
-            <th>Option3</th>
-            <th>Option4</th>
-            <th>Answer</th>
-          </tr>
-          <tr>
-            <th>
-              <input
-                type='text'
-                onChange={e => this.setState({ Question: e.target.value })}
-              />
-            </th>
-            <th>
-              <input
-                type='text'
-                onChange={e => this.setState({ Option1: e.target.value })}
-              />
-            </th>
-            <th>
-              <input
-                type='text'
-                onChange={e => this.setState({ Option2: e.target.value })}
-              />
-            </th>
-            <th>
-              <input
-                type='text'
-                onChange={e => this.setState({ Option3: e.target.value })}
-              />
-            </th>
-            <th>
-              <input
-                type='text'
-                onChange={e => this.setState({ Option4: e.target.value })}
-              />
-            </th>
-            <th>
-              <input
-                type='text'
-                onChange={e => this.setState({ Answer: e.target.value })}
-              />
-            </th>
-          </tr>
+      <div className="container p-5 border">
+        <form onSubmit={this.addQuiz} style={{ width: '80%' }}>
 
-          <button onClick={this.addQuiz}>Add</button>
-        </div>
+          <div className="form-group">
+            <label for="question">Question</label>
+            <input
+              type='text'
+              onChange={e => this.setState({ Question: e.target.value })}
+              value={this.state.Question}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label for="option1">Option1</label>
+            <input
+              type='text'
+              onChange={e => this.setState({ Option1: e.target.value })}
+              value={this.state.Option1}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label for="option2">Option2</label>
+            <input
+              type='text'
+              onChange={e => this.setState({ Option2: e.target.value })}
+              value={this.state.Option2}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label for="option3">Option3</label>
+            <input
+              type='text'
+              onChange={e => this.setState({ Option3: e.target.value })}
+              value={this.state.Option3}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label for="option4">Option4</label>
+            <input
+              type='text'
+              onChange={e => this.setState({ Option4: e.target.value })}
+              value={this.state.Option4}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label for="answer">Answer</label>
+            <input
+              type='text'
+              onChange={e => this.setState({ Answer: e.target.value })}
+              value={this.state.Answer}
+              className="form-control"
+            />
+          </div>
+
+
+          <button type="submit" className="btn btn-success">Submit Question</button>
+
+        </form>
       </div>
+
     );
   }
 }
