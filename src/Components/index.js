317f5e42f9056befc48f512e3e8ec768c324d38a
Donghao Wu <donghao.wu.us@gmail.com>
2019-08-07 11:20:58 -0400

    delete duplicate files and fix todo apps bugs

--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-//This file will be imported in Routes.js
-export {default as GameSection}from "./GameSection";
-export {default as AboutSection}from "./AboutSection";
-export {default as HomeSection}from "./HomeSection";
